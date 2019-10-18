3a50026545ad1b7feeaf74a2dcc41f07f97e6a07
JAVI-CC <javileg98@hotmail.com>
2019-10-18 16:14:51 +0200

    .

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<meta charset="UTF8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<!-- FONT AWESOME  -->
+	<script src="https://kit.fontawesome.com/99d3a05b47.js" crossorigin="anonymous"></script>
+</head>
+<title>PHP MYSQL CRUD</title>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+	<div class="container">
+		<a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+	</div>
+</nav>
